a17cbb09d9f7d4ab07873c38024402db6cc1a29f
Caio Lins <caio.henrique.c.lins@accenture.com>
2025-08-14 15:34:28 -0300

    init java and springboot backend project

---
+++ backend/ohmybills/src/test/java/br/com/core/ohmybills/OhmybillsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.core.ohmybills;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OhmybillsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
