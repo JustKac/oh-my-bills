92220fe9acfe8186c2da6648bb9f8d88eeb9c9a8
Caio Lins <caiohrick@outlook.com>
2025-09-05 14:23:37 -0300

    refactor: rename totalIncomeByYearMonth and totalExpenseByYearMonth to totalIncome and totalExpense in MainPageDTO

--- backend/ohmybills/src/main/java/br/com/core/ohmybills/dto/MainPageDTO.java
+++ backend/ohmybills/src/main/java/br/com/core/ohmybills/dto/MainPageDTO.java
@@ -4,8 +4,8 @@ import java.math.BigDecimal;
 import java.util.Map;
 
 public record MainPageDTO(
-        BigDecimal totalIncomeByYearMonth,
-        BigDecimal totalExpenseByYearMonth,
+        BigDecimal totalIncome,
+        BigDecimal totalExpense,
         BigDecimal totalRecurrence,
         Map<String, BigDecimal> totalExpenseByCreditCard,
         Map<String, BigDecimal> totalExpenseByTag
